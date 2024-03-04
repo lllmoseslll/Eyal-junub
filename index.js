2586c2b89c4c88447773e949f3f8baccfe8dc17d
lllmoseslll <mosespeter620@gmail.com>
2024-03-04 19:25:17 +0300

    v one

---
+++ index.js
@@ -0,0 +1,34 @@
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.get('', (req, res) => {
+    res.render("");
+});
+
+
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+} );
